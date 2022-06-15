9b5a9807600f449560ba6e019eb324b002073445
Sohanur Rahman <86511149+sohan964@users.noreply.github.com>
2022-06-15 15:43:35 +0600

    laptest
    by rafat vai

--- laptest.cpp
+++ laptest.cpp
@@ -9,21 +9,19 @@ class cart{
     int Price_per_Unit[s];
   public:
     string Customer_Name;
-    void getinfo(string cname,string pname,int pquantiti,int pprise);
+    void getinfo();
     int TotalAmount();
 };
-void cart::getinfo(string cname,string pname[s],int pquantiti[s],int pprise[s]){
-    Customer_Name=cname;
+void cart::getinfo(){
+    cin>>Customer_Name;
     for(int i=0;i<s;i++){
-        ProductName[i]=pname[i];
-        ProductQuantitis[i]=pquantiti[i];
-        Price_per_Unit[i]=pprise[i];
+        cin>>ProductName[i]>>ProductQuantitis[i]>>Price_per_Unit[i];
     }
 }
 int cart::TotalAmount(){
-    int total;
+    int total=0;
     for(int i=0;i<s;i++){
-       total= Price_per_Unit[i]*ProductQuantitis[i];
+       total=total + Price_per_Unit[i]*ProductQuantitis[i];
     }
     return total;
 }
@@ -31,7 +29,8 @@ int cart::TotalAmount(){
 int main()
 {
     cart c1;
-    c1.getinfo("Saadman",)
+    c1.getinfo();
+    cout<<c1.TotalAmount();
 
     return 0;
 }
